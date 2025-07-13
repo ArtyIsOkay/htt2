8e0af81a5e421adcb57151fe6479666e7029f118
ArtyIsOkay <alaskaartem@gmail.com>
2025-07-13 22:57:46 +0700

    Заменил фласк на простой сервер

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-@app.route("/")
-def home():
-    return "<h1>Привет, Flask работает!</h1>"
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<h1>Это мой сервер!</h1><p>Порт 8000</p>", "utf-8"))
 
-@app.route("/api/hello")
-def hello():
-    return {"message": "Hello from Flask API!"}
-
-if __name__ == "__main__":
-    app.run(port=8000)  # Запуск на порту 8000
+server = HTTPServer(("localhost", 8000), MyServer)
+server.serve_forever()
