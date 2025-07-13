0c3be684f5ca909f3b35c34469d4fcc5c14e7b4e
ArtyIsOkay <alaskaartem@gmail.com>
2025-07-13 23:00:32 +0700

    Изменил выводимый текст с русского на английский

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<h1>Это мой сервер!</h1><p>Порт 8000</p>", "utf-8"))
+        self.wfile.write(bytes("<h1>This is my server!</h1><p>Port 8000</p>", "utf-8"))
 
 server = HTTPServer(("localhost", 8000), MyServer)
 server.serve_forever()
