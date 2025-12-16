7ff36170cb270d91b9c4b5845b5d867b5a8ba8fb
ArtyIsOkay <alaskaartem@gmail.com>
2025-12-16 21:52:54 +0700

    Выполненные упражнения по циклам

---
+++ cycles/exercise_5.py
@@ -0,0 +1,18 @@
+for x in range (6):
+    print("Обожаю эту программу!")
+print("")
+
+for number in range(6):
+    print(number)
+print("")
+
+for number in range (2, 6):
+    print(number)
+print("")
+
+for number in range (0, 501, 100):
+    print(number)
+print("")
+
+for number in range (10, 5, -1):
+    print(number)
